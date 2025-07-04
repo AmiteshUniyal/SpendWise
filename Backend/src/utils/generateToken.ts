d307efde765f45bfe627dd54b553be6e18fadd62
Amitesh Uniyal <amiteshuniyalz7@gmail.com>
2025-07-05 00:56:29 +0530

    fixed the env variables

--- Backend/src/utils/generateToken.ts
+++ Backend/src/utils/generateToken.ts
@@ -7,7 +7,7 @@ export const generateTokenAndSetCookie = (userId: string, res: Response): void =
 
   res.cookie("jwtToken", token, {
     httpOnly: true, 
-    secure: process.env.NODE_ENV == "production",
+    secure: process.env.NODE_ENV == "development",
     sameSite: "none",
     maxAge: 10 * 24 * 60 * 60 * 1000,
   });
