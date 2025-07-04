0c0739ddbc1bb7ca11606ff2ff68ff5c18cb8d2b
Amitesh Uniyal <amiteshuniyalz7@gmail.com>
2025-07-05 01:34:18 +0530

    setting the PORT in env file

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -36,8 +36,7 @@ app.get('/', (_, res) => {
     res.send(" the rooooooook!!!!!!!!");
 });
 
-const PORT = 8080;
 
 ConnectDB().then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
 });
