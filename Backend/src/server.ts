ed8dae2f6e7f6a8a7d7b7eca060db5e1c3a80c65
Amitesh Uniyal <amiteshuniyalz7@gmail.com>
2025-07-05 02:11:43 +0530

    setting CORS correctly

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: process.env.FRONTEND_URL,
         methods: ["POST", "GET", "DELETE", "PUT"],
         credentials: true,
     })
